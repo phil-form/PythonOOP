f24a2e5ca3a9077c898148d4aa3cf6f8433ff9b5
clbr-odoo <clbr@odoo.com>
2022-06-20 10:53:21 +0200

    [MVC] Fancier person display

--- Exercices/Claire/03_MVC/app/models/Person.py
+++ Exercices/Claire/03_MVC/app/models/Person.py
@@ -1,9 +1,12 @@
 class Person:
 
-    def __init__(self, first_name, last_name, age):
+    def __init__(self, first_name, last_name):
         self.__first_name = first_name
         self.__last_name = last_name
-        self.__age = age
+        self.__descr = None
+        self.__experiences = []
+        self.__skills = []
+        self.__training = None
 
     @property
     def first_name(self):
@@ -14,5 +17,31 @@ class Person:
         return self.__last_name
 
     @property
-    def age(self):
-        return self.__age
+    def descr(self):
+        return self.__descr
+
+    @descr.setter
+    def descr(self, value: str):
+        self.__descr = value
+
+    @property
+    def experiences(self):
+        return self.__experiences
+
+    def addExperience(self, time: str, descr: str):
+        self.experiences.append({"time": time, "descr": descr})
+
+    @property
+    def skills(self):
+        return self.__skills
+
+    def addSkill(self, descr: str):
+        self.skills.append(descr)
+
+    @property
+    def training(self):
+        return self.__training
+
+    @training.setter
+    def training(self, value: str):
+        self.__training = value
