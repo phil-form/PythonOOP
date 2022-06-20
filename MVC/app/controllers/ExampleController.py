a4dfdde2e55850ab884b5bb7267e0be96040c888
philippe.stons <philippe.stons@bstorm.be>
2022-06-20 12:23:25 +0200

    [FORM] example form and POST

--- MVC/app/controllers/ExampleController.py
+++ MVC/app/controllers/ExampleController.py
@@ -1,11 +1,26 @@
+from requests import request
 from app import app
-from flask import render_template
+from flask import render_template, request
+
+from app.forms.ExampleForm import ExampleForm
 
 class ExampleController:
     @app.route('/')
     def example():
         return render_template('example/example.html')
 
-    @app.route('/example')
+    # @app.route('/example', methods=['GET', 'POST'])
+    @app.route('/example', methods=['GET'])
     def exampleExample():
-        return render_template('example/superExample.html', example="C'est un test")
+        # if request.method == 'POST':
+        #     return render_template('example/superExample.html', example="POST")
+        return render_template('example/superExample.html', example="GET")
+
+    @app.route('/example', methods=['POST'])
+    def examplePostExample():
+        form = ExampleForm(request.form)
+
+        if form.validate():
+            return render_template('example/superExample.html', example=f"{form.firstname.data} {form.lastname.data}")
+
+        return render_template('example/superExample.html', example="POST")
