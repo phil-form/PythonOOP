004f16d63ad46fab440337713245f8444a256ef0
clbr-odoo <clbr@odoo.com>
2022-06-21 09:33:14 +0200

    [MVC] Contact with DB

--- Exercices/Claire/03_MVC/app/__init__.py
+++ Exercices/Claire/03_MVC/app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
+import psycopg2
 
 app = Flask("app")
 app.config['SECRET_KEY'] = 'random'
 app.debug = True
 
 toolbar = DebugToolbarExtension(app)
+
+conn = psycopg2.connect(dbname='technobelapp', user='postgres', password='postgres', host='127.0.0.1', port='5432')
+print(conn)
 
 from app.controllers import *
